2916dff7f11cf9bf38c3d598afa2124c02a6bf07
SichengYang <sicheng@wustl.edu>
2025-07-07 14:49:09 -0500

    edgeless app creation

---
+++ main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow, Menu, ipcMain } = require('electron/main')
+const path = require('node:path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        frame: false, // Removes window edges and title bar
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    ipcMain.handle('ping', () => 'pong')
+    createWindow()
+})
